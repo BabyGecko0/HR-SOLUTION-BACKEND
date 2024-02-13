a788b1f7d4b57800b9221cb03a3217262fe6bae3
BabyGecko0 <malasialeks1@gmail.com>
2024-02-13 16:30:01 +0100

    first java commit

---
+++ hr/hr/src/test/java/hr_java/HrJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
